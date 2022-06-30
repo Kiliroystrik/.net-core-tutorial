1c675406ee89498e24e6aa0508b2a0a3eef22f61
Kiliroy <borges.mathieu@gmail.com>
2022-06-30 11:24:46 +0200

    Added inline attributes

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,30 +6,22 @@ namespace MvcMovie.Models
     public class Movie
     {
 
-        //ID
         public int Id { get; set; }
 
-        //TITLE
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string? Title { get; set; }
 
-        //RELEASE DATE
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        //GENRE
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [Required]
-        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string? Genre { get; set; }
 
-        //RATING
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
+        [Range(1, 100), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string? Rating { get; set; }
     }
 }
